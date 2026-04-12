5ede5977c86ba6ab1e44514a7858e1f50a65d42c
ecarder2git <ecarder2@illinois.edu>
2026-04-12 15:59:17 -0500

    Update train.sh
    Adding saving interval

--- scripts/train.sh
+++ scripts/train.sh
@@ -16,7 +16,7 @@ for seed in 1; do
   XLA_PYTHON_CLIENT_MEM_FRACTION=.95 MUJOCO_GL=egl CUDA_VISIBLE_DEVICES=0 python run.py "$method" \
     --wandb_project_name test --wandb_group first_run --exp_name test --num_evals 50 \
     --seed ${seed} --total_env_steps 10000000 --batch_size 256 --num_envs 512 \
-    --discounting 0.99 --action_repeat 1 --env ${env} --checkpoint_logdir "checkpoints/${seed}" \
+    --discounting 0.99 --action_repeat 1 --env ${env} --checkpoint_logdir "checkpoints/${seed}" --save_interval 5 \
     --episode_length 1000 --unroll_length 62  --min_replay_size 1000 --max_replay_size 10000 \
     --contrastive_loss_fn bwd_infonce --energy_fn norm \
     --train_step_multiplier 1 --log_wandb 
